72aa75745e4a942be5c21c25d11bea60ea93f577
Benjamin KABEYA **Afrik Interim HR Services ** <benjamin.kabeya@afrikinterim.local>
2025-01-10 17:47:24 +0100

    feat: add of async function to get and post new user

--- Back-end/controllers/users.js
+++ Back-end/controllers/users.js
@@ -1,17 +1,68 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 
 //recupération de tous les utilisateurs
-const getUsers = (req,res)=>{
-    res.status(200).json({message:`Liste de tous utilisateurs`})
+const getUsers = async (req,res)=>{
+
+    try {
+        const users = await prisma.user.findMany()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(200).json({message:`Erreur lors de la récupération des données dans le serveur`})
+    }
 }
 
 //recupération d'un utilisateur 
-const getUser = (req,res)=>{
-    res.status(200).json({message:`Liste de tous utilisateurs n° ${req.params.id}`})
+const getUser = async (req,res)=>{
+   
+    try {
+        const user = await prisma.user.findUnique({
+            where:{
+                id: req.pramas.id
+            }
+        })
+
+        if(!user){
+            res.status(404).json({message:`Utilisateur non trouvé`})
+        }
+        res.status(200).json(user)
+
+    } catch (error) {
+        
+        res.status(500).json({message:`Erreur lors de la connexion au serveur`})
+    }
 }
 
 //création d'un nouvelle utilisateur
-const postUsers = (req,res)=>{
-    res.status(200).json({message:`Création d'un nouvel utilisateur reussi !`})
+const postUsers = async(req,res)=>{
+    const { firstName, lastName, email, password, phone } = req.body;
+  try {
+    if(!firstName || !lastName || !email || !password || !phone){
+        res.status(400).json({message:` Veuillez remplir tous les champs`})
+    }
+
+    const newUser = await prisma.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          password,
+          phone,
+        },
+      });
+  
+      res
+        .status(201)
+        .json({ message: "Utilisateur ajouté avec succès", user: newUser });
+
+
+  } catch (error) {
+    res.status(400).json({
+      message: "Erreur lors de l'ajout de l'utilisateur",
+      error: error.message,
+    });
+  }
 }
 
 //Modification d'un utilisateur 
