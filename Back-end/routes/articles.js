f750b1e3e4e6b1721a64ebe39a9b37a4010a3672
Benjamin KABEYA **Afrik Interim HR Services ** <benjamin.kabeya@afrikinterim.local>
2025-01-10 09:39:24 +0100

    fix: modified routes structure in articles.js

--- Back-end/routes/articles.js
+++ Back-end/routes/articles.js
@@ -2,11 +2,8 @@ const express = require("express")
 const articleRoutes = express.Router()
 const {getArticle, getArticles, postArticle, putArticle,patchArticle,deleteArticle} = require("../controllers/articles")
 
-articleRoutes.route("/").get(getArticles)
-articleRoutes.route("/:id").get(getArticle)
-articleRoutes.route("/").post(postArticle)
-articleRoutes.route("/:id").put(putArticle)
-articleRoutes.route("/:id").patch(patchArticle)
+articleRoutes.route("/").get(getArticles).post(postArticle)
+articleRoutes.route("/:id").get(getArticle).put(putArticle).patch(patchArticle)
 articleRoutes.route("/:id").delete(deleteArticle)
 
 
